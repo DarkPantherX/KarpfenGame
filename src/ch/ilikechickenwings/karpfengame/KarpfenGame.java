565a0a77add4b1ca941599dfa7f6d455c73a900a
DarkPantherX <Dark-Panther@gmx.ch>
2013-10-13 18:08:46 +0200

    Cleaned up Code - Changed ArrayList wzombies to entities

--- src/ch/ilikechickenwings/karpfengame/KarpfenGame.java
+++ src/ch/ilikechickenwings/karpfengame/KarpfenGame.java
@@ -3,140 +3,72 @@ package ch.ilikechickenwings.karpfengame;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
-import java.util.ArrayList;
-import java.util.Random;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
-import ch.ilikechickenwings.karpfengame.Entity.Player;
 import ch.ilikechickenwings.karpfengame.Handler.ComponentHandler;
 import ch.ilikechickenwings.karpfengame.Handler.InputHandler;
 import ch.ilikechickenwings.karpfengame.Menu.Menu;
 
-
-public class KarpfenGame extends JPanel implements Runnable{
+public class KarpfenGame extends JPanel implements Runnable {
 
 	/**
-	 *  08.10.2013 - Strichcoder joined the project
-	 *  09.10.2013 - Space joined the project
-	 *  09.10.2013 - great another idiot...
-
-
-	 *  09.10.2013 - I should concentrate on my studies...
-
+	 * 08.10.2013 - Strichcoder joined the project 09.10.2013 - Space joined the
+	 * project 09.10.2013 - great another idiot...
+	 * 
+	 * 
+	 * 09.10.2013 - I should concentrate on my studies...
 	 */
+
+	
+	public static int WIDTH = 640;
+	public static int HEIGHT = 480;
+	public static boolean paused = false;
 	
 	private static final long serialVersionUID = 1L;
-	public static int WIDTH=640;
-	public static int HEIGHT=480;
-	//public static ArrayList<Wall> walls = new ArrayList<Wall>(); // moved to Level.java - SC
-	public static boolean paused=false;
+	
 	private InputHandler inHandler;
 	private ComponentHandler compHandler;
 	private Menu menu;
-	private long oldTime=System.currentTimeMillis();
-	//public static Player player; // moved to Level.java - SC
 	private Level lvl;
-	private boolean a[] = new boolean[8];
+	private long oldTime = System.currentTimeMillis();
 	
 	
-	public KarpfenGame(){
-		
+	
+
+	public KarpfenGame() {
+
 		// makes new listener (ComponentHandler and InputHandler) and add them
-				// to the game
-				inHandler = new InputHandler();
-				compHandler = new ComponentHandler();
-				addKeyListener(inHandler);
-				addComponentListener(compHandler);
-				// sets the menu to the IntroScreenMenu (at the begin of the game)
-				setMenu(new Menu());
+		// to the game
+		inHandler = new InputHandler();
+		compHandler = new ComponentHandler();
+		addKeyListener(inHandler);
+		addComponentListener(compHandler);
+		// sets the menu to the IntroScreenMenu (at the begin of the game)
+		setMenu(new Menu());
 
-				// Player player = new Player(0,0,10,30,70);
-				// this.player = player; // moved to Level.java - SC
-				this.lvl = new Level(1,this);
-				// start the game thread (run-method)
-				new Thread(this).start();
+		this.lvl = new Level(1, this);
+		// start the game thread (run-method)
+		new Thread(this).start();
 	}
-	
-
-
-		@Override
-	public void run() {
-		
-			while(!paused){
-			update();
-			repaint();
-		}
-		
-	}
-	
-		/*
-	private void createWalls(boolean first) {
-		if(first){
-			Wall wall = new Wall(0,230,80,20);
-			walls.add(wall);
-		}else if(wid>0){
-				int in = walls.size()-1;
-				Wall wi = (Wall) walls.get(in);
-				Random r= new Random();
-				int x=(int) (r.nextInt(100-30)+30)+wi.getX_Point()+wi.getWidth();
-				int y=getWallY(wi);
-				int xy=(int) (r.nextInt(100-30)+30);
-				int yy=20;
-				wid= wid-xy;
-				Wall wall= new Wall(x,y,xy,yy);
-				walls.add(wall);
-			}
-			
-		}
-
-	private int getWallY(Wall wi){
-		Random r = new Random();
-		int a=(int) (r.nextInt(240)-120)+wi.getY_Point()+wi.getHeight();
-		
-		if(a>HEIGHT-50||a<50){
-					
-			a=getWallY(wi);
-				}
-		return a;
-		
-	}
-    */
 
 	private void update() {
-		
+
 		lvl.update(inHandler);
-		long timeElapsed=System.currentTimeMillis()-oldTime;
-		//System.out.println(timeElapsed);
-		if(timeElapsed<40)
-		{
+		long timeElapsed = System.currentTimeMillis() - oldTime;
+		// System.out.println(timeElapsed);
+		if (timeElapsed < 40) {
 			try {
-			    Thread.sleep((long) 40-timeElapsed); // ~25fps
-		    } catch (InterruptedException e) {
-			    System.out.println("Couldn't start sleeping:" + e);
-			    e.printStackTrace();
-		    }
-		}	
-		oldTime=System.currentTimeMillis();	
-			
-		/* walls dont need to change on their own anymore
-		for (int w = 0; w < walls.size(); w++) {
-			Wall wall = (Wall) walls.get(w);
-			
-			wall.setX_Point(wall.getX_Point()-2);
-			if(wall.getX_Point()+wall.getWidth()<0){
-				walls.remove(wall);
-				wid=wid+wall.getWidth();
+				Thread.sleep((long) 40 - timeElapsed); // ~25 updates per second
+			} catch (InterruptedException e) {
+				System.out.println("Couldn't start sleeping:" + e);
+				e.printStackTrace();
 			}
-		
-		 }
-		 */
-		//getA();
-		
-		// player.update(inHandler); // moved to Level.java - SC
-		
 		}
+		oldTime = System.currentTimeMillis();
+
+	}
 
 	public void paintComponent(Graphics g) {
 		Graphics2D g2 = (Graphics2D) g;
@@ -144,26 +76,19 @@ public class KarpfenGame extends JPanel implements Runnable{
 		g2.fillRect(0, 0, WIDTH, HEIGHT);
 
 		lvl.draw(g2);
-		/* moved to Level.java - SC
-		for (int w = 0; w < walls.size(); w++) {
-			g2.setColor(Color.red);
-			Wall wall = (Wall) walls.get(w);
-			//System.out.println(w);
-			//System.out.println(wall.getX_Point() + " " + wall.getY_Point()
-			//		+ " " + wall.getWidth() + " " + wall.getHeight());
-			g2.fillRect(wall.getX_Point(), wall.getY_Point(), wall.getWidth(),
-					wall.getHeight());
-			player.draw(g2);
-		
-		}
-		*/
+
 	}
 
-	
-	
-	
-	
-	
+	@Override
+	public void run() {
+
+		while (!paused) {
+			update();
+			repaint();
+		}
+
+	}
+
 	public void addNotify() {
 		super.addNotify();
 		requestFocus();
@@ -186,68 +111,17 @@ public class KarpfenGame extends JPanel implements Runnable{
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 
-			private void setMenu(Menu menu) {
-		this.menu=menu;
-		
+	private void setMenu(Menu menu) {
+		this.menu = menu;
+
 	}
 
+	public Level getLvl() {
+		return lvl;
+	}
 
+	public void setLvl(Level lvl) {
+		this.lvl = lvl;
+	}
 
-			public Level getLvl() {
-				return lvl;
-			}
-
-
-
-			public void setLvl(Level lvl) {
-				this.lvl = lvl;
-			}
-
-		/**	public boolean[] getA() {
-
-				if (inHandler.getKeys()[KeyEvent.VK_W]) {
-					a[0] = true;
-
-				}
-				if (inHandler.getKeys()[KeyEvent.VK_S]) {
-					a[1] = true;
-
-				}
-				if (inHandler.getKeys()[KeyEvent.VK_A]) {
-					System.out.println("asdf");
-					a[2] = true;
-
-				}
-				if (inHandler.getKeys()[KeyEvent.VK_D]) {
-					a[3] = true;
-
-				}
-
-				if (inHandler.getKeys()[KeyEvent.VK_UP]) {
-					a[8] = true;
-				}
-
-				if (inHandler.getKeys()[KeyEvent.VK_DOWN]) {
-					a[9]=true;
-				}
-
-				if (inHandler.getKeys()[KeyEvent.VK_SPACE]) {
-					a[4] = true;
-
-				}
-				if (inHandler.getKeys()[KeyEvent.VK_ENTER]) {
-					a[5] = true;
-				}
-
-				if (inHandler.getKeys()[KeyEvent.VK_ESCAPE]) {
-					a[6] = true;
-				}
-				if (inHandler.getKeys()[KeyEvent.VK_L]) {
-					a[7] = true;
-				}
-
-				return a;
-			}
-**/}
-
-			
+}
