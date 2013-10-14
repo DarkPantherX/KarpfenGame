fb529c26370052cb4e6c6a4e23738f945d0ca597
Strichcoder <raphael.api@bluewin.ch>
2013-10-14 06:07:25 +0200

    Increased the width and height of Coffee.java - Added a secret feature

---
+++ src/ch/ilikechickenwings/karpfengame/Entity/Item/Coffee.java
@@ -0,0 +1,42 @@
+package ch.ilikechickenwings.karpfengame.Entity.Item;
+
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.Toolkit;
+
+import ch.ilikechickenwings.karpfengame.Entity.Item.Item;
+
+public class Coffee extends Item{
+	
+	private int caffeine;
+	
+	public Coffee(int x, int y){
+		setWidth(30);
+		setHeight(30);
+		setX_Point(x-(getWidth()/2));
+		setY_Point(y-getHeight());
+		
+		setCaffeine(10); // may has to be changed
+	}
+
+	public void draw(Graphics2D g2, int xOffset){
+		
+		Image imge1 = null;
+		imge1 = Toolkit.getDefaultToolkit().getImage(
+				getClass().getResource("/res/java-logo[1].gif")); // not declared to use for commercial uses..
+	    g2.drawImage(imge1,getX_Point()-xOffset,getY_Point(), getWidth(), getHeight(),null);
+		
+	}
+
+	public int getCaffeine() {
+		return caffeine;
+	}
+
+	public void setCaffeine(int caffeine) {
+		this.caffeine = caffeine;
+	}
+	
+	
+	
+}
