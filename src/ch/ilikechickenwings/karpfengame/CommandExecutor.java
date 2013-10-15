74673638ae7fb2a9a3ae4bfe320ec17218d3a2ce
DarkPantherX <Dark-Panther@gmx.ch>
2013-10-15 13:27:48 +0200

    Added Commands

--- src/ch/ilikechickenwings/karpfengame/CommandExecutor.java
+++ src/ch/ilikechickenwings/karpfengame/CommandExecutor.java
@@ -2,19 +2,52 @@ package ch.ilikechickenwings.karpfengame;
 
 public class CommandExecutor {
 
-	public CommandExecutor(String str, Level lv){
-		if(str.equals("kill")){
-			lv.player.setLifes(0);
-		}else if(str.equals("reset")){
-			lv.getKarpfenGame().setLvl(new Level(1,lv.getKarpfenGame()));
-		}else if(str.equals("clearcoffe")){
-			lv.player.setCoffee(0);
-		}else if(str.equals("fillup")){
-			lv.player.setCoffee(100);
-			lv.player.setLifes(100);
-		}
+	public CommandExecutor(String[] str2, Level lv){
+		boolean sec=str2.length>1;
 		
+		if(str2[0].equals("player")){
+			if(sec){
+				if(str2[1].equals("getHealth")){
+					System.out.println("Health: "+ lv.player.getLifes());
+				}else if(str2[1].equals("kill")){
+			lv.player.setLifes(0);
+			System.out.println("Killed Player");
+				}
+			}
+		}else if(str2[0].equals("clear")){
+			if(sec){
+			if(str2[1].equals("level")){
+			lv.getKarpfenGame().setLvl(new Level(1,lv.getKarpfenGame()));
+			System.out.println("Level has been reset");
+			}else if(str2[1].equals("entities")){
+				Level.entities.clear();
+				System.out.println("Entities has been reset");
+			}else if(str2[1].equals("walls")){
+				Level.walls.clear();
+				System.out.println("Walls has been reset");
+			}
+			}
+		}else if(str2[0].equals("clearcoffee")){
+			lv.player.setCoffee(0);
+			System.out.println("Coffe is now empty!");
+		}else if(str2[0].equals("fillup")){
+			if(sec){
+			if(str2[1].equals("coffee")){
+			lv.player.setCoffee(100);
+			System.out.println("Filled up coffee");
+			}else if(str2[1].equals("life")){
+			lv.player.setLifes(100);
+			System.out.println("Filled up life");
+			}
+				lv.player.setCoffee(100);
+				lv.player.setLifes(100);
+			System.out.println("Filled up player");
+			
+			}
+			
+		}else if(str2[0].equals("help")){
+			System.out.println("List of Commands:" +"\n"+"kill"+"\n"+"clear <level/entities/walls>"+"\n"+"fillup <coffee/health>"+ "\n"+"playerhealth"+"\n"+"playercoffee <coffee/health>" );
+		}
+	
 	}
-	
-	
 }
