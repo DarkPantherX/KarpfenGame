7c47dfab8484305c63e46bdda5b1714ac1f70e34
DarkPantherX <Dark-Panther@gmx.ch>
2013-11-14 10:56:09 +0100

    Animations to come!

---
+++ src/ch/ilikechickenwings/karpfengame/AnimatedTile.java
@@ -0,0 +1,28 @@
+package ch.ilikechickenwings.karpfengame;
+
+import java.awt.image.BufferedImage;
+
+public class AnimatedTile {
+	
+	public static BufferedImage[] getAnimation(BufferedImage img, int row, int col, int width, int height){
+		BufferedImage[] animation = new BufferedImage[row*col];
+		
+		for (int i = 0; i < row; i++)
+		{
+		    for (int j = 0; j < col; j++)
+		    {
+		        animation[(i * col) + j] = img.getSubimage(
+		            j * width,
+		            i * height,
+		            width,
+		            height
+		        );
+		    }
+		}
+		
+		
+		
+		return animation;
+	}
+
+}
--- src/ch/ilikechickenwings/karpfengame/Tile.java
+++ src/ch/ilikechickenwings/karpfengame/Tile.java
@@ -2,16 +2,23 @@ package ch.ilikechickenwings.karpfengame;
 
 import java.awt.Image;
 import java.awt.Toolkit;
+import java.awt.image.BufferedImage;
 
 public class Tile {
 	public static Image coffee;
 	public static Image healthpacks;
 	public static Image platform;
+	public static BufferedImage[] player;
 	
 	
 	public Tile(){
 		coffee= Toolkit.getDefaultToolkit().getImage(
 				getClass().getResource("/res/java-logo[1].gif"));
+		
+		BufferedImage playerSheet= (BufferedImage) Toolkit.getDefaultToolkit().getImage(
+				getClass().getResource("/res/java-logo[1].gif"));
+		
+		player= AnimatedTile.getAnimation(playerSheet, 3, 3, 30, 50);
 		
 	}
 	
