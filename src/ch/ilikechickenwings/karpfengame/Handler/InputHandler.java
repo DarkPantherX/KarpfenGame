692549060af7f590017f09b37d96228a25a91b7d
unknown <Appenzeller@.(none)>
2014-02-12 04:11:26 +0100

    Almost nothing

--- src/ch/ilikechickenwings/karpfengame/Handler/InputHandler.java
+++ src/ch/ilikechickenwings/karpfengame/Handler/InputHandler.java
@@ -6,8 +6,6 @@ import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.util.ArrayList;
 
-import ch.ilikechickenwings.karpfengame.Wall;
-
 public class InputHandler implements KeyListener, MouseListener {
 	
 	public boolean keys[] = new boolean[500];
@@ -41,8 +39,9 @@ public class InputHandler implements KeyListener, MouseListener {
 	// MOUSE
 	
 	@Override
-	public void mouseClicked(MouseEvent e){
+	public void mousePressed(MouseEvent e){
 		mouse.add(e.getButton());
+		System.out.println("Heello");
 	}
 	
 	@Override
@@ -125,7 +124,7 @@ public class InputHandler implements KeyListener, MouseListener {
 	}
 
 	@Override
-	public void mousePressed(MouseEvent arg0) {
+	public void mouseClicked(MouseEvent arg0) {
 		// TODO Auto-generated method stub
 		
 	}
