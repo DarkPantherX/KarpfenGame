4c837c1e0e67d3833492d8116fdf1bb83b1fd6f5
DarkPantherX <Dark-Panther@gmx.ch>
2014-05-15 01:32:07 +0200

    Fixed VisionObstructor moving with offset

--- src/ch/ilikechickenwings/karpfengame/Entity/VisionObstructer.java
+++ src/ch/ilikechickenwings/karpfengame/Entity/VisionObstructer.java
@@ -6,39 +6,35 @@ import java.awt.Image;
 
 import ch.ilikechickenwings.karpfengame.Level;
 import ch.ilikechickenwings.karpfengame.Handler.InputHandler;
+import ch.ilikechickenwings.karpfengame.Handler.Timer;
 
 public class VisionObstructer extends Entity{
 	
 	
-	private long shitCoolDown;
-	private boolean firstTimeShit=true;
 	private Image img;
+	private Timer timer;
 	
 	public VisionObstructer(int x, int y, Image img){
 		setLifes(1);
 		setX_Point(x);
 		setY_Point(y);
 		this.img=img;
+		timer = new Timer(3000);
 	}
 	
 	
 	public void update(InputHandler inhandler){
-		long currTime = System.currentTimeMillis();
 
-		if (firstTimeShit) {
-			shitCoolDown = currTime;
-			firstTimeShit = false;
-
-		} else if (shitCoolDown + 3000 < currTime) {
-			firstTimeShit = true;
+		if (timer.isReady()) {
 			Level.entities.remove(this);
+			
 		}
-			}
+	}
 		
 	
 	
 	public void draw(Graphics2D g, int xOffset) {
-		g.drawImage(img, getX_Point()-xOffset, getY_Point(),250,250, null);
+		g.drawImage(img, getX_Point(), getY_Point(),250,250, null);
 	}
 
 }
