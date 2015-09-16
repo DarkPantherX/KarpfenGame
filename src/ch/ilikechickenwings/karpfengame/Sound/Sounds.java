47e41beaa9ccf44c3745c772243817b72d96c61d
unknown <Timothe@.(none)>
2015-09-16 18:23:27 +0200

    Added new Sound

--- src/ch/ilikechickenwings/karpfengame/Sound/Sounds.java
+++ src/ch/ilikechickenwings/karpfengame/Sound/Sounds.java
@@ -7,11 +7,14 @@ public class Sounds {
 	public static URL playerShot;
 	public static URL gameMusic1;
 	public static URL gameMusic2;
+	public static URL bossSpawnSound;
 	
 		public Sounds(){
 		playerShot =getClass().getResource("/Res/shot.wav");
 		gameMusic1 =getClass().getResource("/Res/Game1.wav");
 		gameMusic2 =getClass().getResource("/Res/Game2.wav");
+		
+		bossSpawnSound =getClass().getResource("/Res/bossSpawn.wav");
 		
 	}
 
