b1694d75b2f9dbdfe2018cb07ffc1fe596fa59b0
unknown <Timothe@.(none)>
2015-09-16 18:23:16 +0200

    Added new timer function ->get the progression

--- src/ch/ilikechickenwings/karpfengame/Handler/Timer.java
+++ src/ch/ilikechickenwings/karpfengame/Handler/Timer.java
@@ -4,17 +4,21 @@ public class Timer {
 
 	private long startMilli;
 	private int millis;
-	
-	public Timer(int millis){
+
+	public Timer(int millis) {
 		setMillis(millis);
 		setStartMilli(System.currentTimeMillis());
 	}
 
-	public boolean isReady(){
-		long milli= (long)getMillis();
-		return getStartMilli()+milli < System.currentTimeMillis();
+	public double getProgress() {
+		return (((double) millis) - (System.currentTimeMillis() - startMilli)) / (double) millis;
 	}
-	
+
+	public boolean isReady() {
+		long milli = (long) getMillis();
+		return getStartMilli() + milli < System.currentTimeMillis();
+	}
+
 	private long getStartMilli() {
 		return startMilli;
 	}
@@ -30,6 +34,5 @@ public class Timer {
 	private void setMillis(int millis) {
 		this.millis = millis;
 	}
-	
-	
+
 }
